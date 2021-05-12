7c4a8f5c74f2c2d4ea1193ce5a75f42331112555
ashinde3 <ashinde3@binghamton.edu>
2021-05-11 20:05:15 -0400

    Models and Controllers updated

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,12 +1,12 @@
 import express from 'express';
+import {getPosts, createPost} from '../controllers/posts.js';
 
 const router = express.Router();
 
 
 // http://localhost:5000/posts
 
-router.get('/', (req,res) => {
-    res.send('This ROUTING works');
-});
+router.get('/', getPosts);
+router.post('/', createPost);
 
 export default router;
