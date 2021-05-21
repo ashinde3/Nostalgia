6a3738256f80f5938e2df3fbf002286bbdd31a33
ashinde3 <ashinde3@binghamton.edu>
2021-05-21 12:04:45 -0400

    Added Google authentication

--- frontend/src/components/Auth/Auth.js
+++ frontend/src/components/Auth/Auth.js
@@ -1,11 +1,93 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {Avatar, Button, Paper, Grid, Typography, Container} from '@material-ui/core';
+import useStyles from './styles';
+import LockIcon from '@material-ui/icons/LockOutlined';
+import Input from './Input';
+import {GoogleLogin} from 'react-google-login';
+import Icon from './icon';
 
 const Auth = () => {
-    const state = null;
+    const classes = useStyles();
+    const [showPassword,setShowPassword] = useState(false);
+    const [isSignup , setIsSignup] = useState(false)
+
+    const handleSubmit = () => {
+
+    }
+
+    const handleChange = () => {
+
+    }
+
+    const googleSuccess = async (res) => {
+        console.log(res);
+    }
+
+    const googleFailure = () => {
+        console.log('Google Sign In failed. Try again.');
+    }
+
+    const switchMode = () => {
+        setIsSignup((prevIsSignup) => !prevIsSignup);
+        handleShowPassword(false);
+    }
+
+    const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
+
+
     return (
-        <div>
-            AUTH
-        </div>
+        <Container component='main' maxWidth='xs'>
+            <Paper className={classes.paper} elevation={3}>
+                <Avatar className={classes.avatar}>
+                    <LockIcon />
+                </Avatar>
+                <Typography variant='h5'>{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {
+                            isSignup && (
+                                <>
+                                    <Input name='firstName' label='First Name' handleChange={handleChange} autoFocus half />
+                                    <Input name='lastName' label='Last Name' handleChange={handleChange} half />
+                                </>
+                            )
+                        }
+                        <Input name='email' label='Email' handleChange={handleChange} type='email' />
+                        <Input name='password' label='Password' handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
+                        {isSignup && <Input name='confirmPassword' label='Repeat Password' handleChange={handleChange} type='password' />}
+                    </Grid>
+                    <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>
+                        {isSignup ? 'Sign up' : 'Sign In'}
+                    </Button>
+                    <GoogleLogin
+                        clientId='46054141457-85bdtkkm1qdkf98a3i52u5jlbsmm7f1u.apps.googleusercontent.com'
+                        render={(renderProps) => (
+                            <Button 
+                                className={classes.googleButton} 
+                                color='primary' 
+                                fullWidth 
+                                onClick={renderProps.onClick} 
+                                disabled={renderProps.disabled} 
+                                startIcon={<Icon />} 
+                                variant='contained' 
+                            >
+                                Sign In with Google
+                            </Button>
+                        )}
+                        onSuccess={googleSuccess}
+                        onFailure={googleFailure}
+                        cookiePolicy='single_host_origin'
+                    />
+                    <Grid container justify='flex-end'>
+                        <Grid item>
+                            <Button onClick={switchMode}>
+                                {isSignup ? 'Already have an account? Sign In' : 'Not a user? Sign Up'}
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </Paper>
+        </Container>
     )
 }
 
